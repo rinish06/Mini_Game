48ca3b92df81538f38e64b09cba6738d2a45fde3
rinish06 <54551163+rinish06@users.noreply.github.com>
2020-02-07 16:01:37 -0600

    Created scripts folder
    Almost done with canon ball

---
+++ Assets/Scripts/CanonWorking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonWorking : MonoBehaviour
+{
+    public GameObject cannonball;
+    public float rateoffire = 1;
+    public float speed = 50;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject clone = Instantiate(cannonball, transform.position, transform.rotation);
+        clone.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3(0,0,speed));
+        Physics.IgnoreCollision(clone.GetComponent<Collider>(), transform.root.GetComponent<Collider>());
+    }
+}
