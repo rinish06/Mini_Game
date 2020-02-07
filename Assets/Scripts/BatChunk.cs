497f1c830f682eec91541edee98b9150271aa9bb
Jacob Petersen <jpetersen6202@gmail.com>
2020-02-07 16:34:11 -0600

    added some scripts for bat
    added some scripts for bat use, still finishing physics.

---
+++ Assets/Scripts/BatChunk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatChunk : MonoBehaviour
+{
+    //[SerializeField]
+	public BatChunkFollower _BatChunkFollowerPrefab;
+
+	public void SpawnBatChunkFollower()
+	{
+		var follower = Instantiate(_BatChunkFollowerPrefab);
+		follower.transform.position = transform.position;
+		follower.SetFollowTarget(this);
+	}
+
+	public void Start()
+	{
+		SpawnBatChunkFollower();
+	}
+}
---
+++ Assets/Scripts/BatChunkFollower.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BatChunkFollower : MonoBehaviour
+{
+    private BatChunk _batFollower;
+	private Rigidbody _rigidbody;
+	private Vector3 _velocity;
+
+	[SerializeField]
+	private float _sensitivity = 100f;
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		Vector3 destination = _batFollower.transform.position;
+		_rigidbody.transform.rotation = transform.rotation;
+
+		_velocity = (destination - _rigidbody.transform.position) * _sensitivity;
+
+		_rigidbody.velocity = _velocity;
+		transform.rotation = _batFollower.transform.rotation;
+	}
+
+	public void SetFollowTarget(BatChunk batFollower)
+	{
+		_batFollower = batFollower;
+	}
+}
