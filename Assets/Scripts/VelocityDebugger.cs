f2c2d2a18eedbab1ac5d1b59d7bd7c23ad3e1777
rinish06 <54551163+rinish06@users.noreply.github.com>
2020-02-10 17:11:50 -0600

    update 1.2
    adfasdfasdfasdf

---
+++ Assets/Scripts/VelocityDebugger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityDebugger : MonoBehaviour
+{
+    private float maxVelocity = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().material.color = ColorForVelocity();
+    }
+
+    private Color ColorForVelocity()
+    {
+        float velocity = GetComponent<Rigidbody>().velocity.magnitude;
+
+        return Color.Lerp(Color.green, Color.red, velocity / maxVelocity);
+    }
+}
