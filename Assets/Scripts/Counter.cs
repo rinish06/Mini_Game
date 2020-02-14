9c94bb8cb2cac4b9c0c80c895c4434afa28163e3
rinish06 <54551163+rinish06@users.noreply.github.com>
2020-02-14 13:48:35 -0600

    Update 2.1
    adfadfasdf

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+    public Text loseText;
+    public int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        SetCountText ();
+        winText.text = ""; 
+        loseText.text = "";  
+    }
+
+    /*void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ( "baseball"))
+        {
+            //other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }*/
+
+    public void SetCountText ()
+    {
+        countText.text = "Targets Hit: " + count.ToString ();
+        if (count >= 5)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+}
